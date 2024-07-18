44ee9d7597c27390d342b5cc442765043c1014bf
Jessica ROUILLON <jrouillo@paul-f4Br5s8.clusters.42paris.fr>
2024-07-18 15:27:41 +0200

    ca fonctionne

--- pong_backend/User_Management/urls.py
+++ pong_backend/User_Management/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import register
 
 urlpatterns = [
-    path('register/', register, name='register'),
+    # path('register/', register, name='register'),
 ]
 
 ### Ajout pour API ###
--- pong_backend/pong_backend/urls.py
+++ pong_backend/pong_backend/urls.py
@@ -18,11 +18,10 @@ Including another URLconf
 ### Ajout pour API ###
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('User_Management.urls')),
 ]
 
 ### Ajout pour API ###
