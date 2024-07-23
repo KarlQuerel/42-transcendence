a2ad7f8d1ed4497001dd390c62a6819c4819f5fc
Clement BERNAZEAU <clem.bernazeau@orange.fr>
2024-07-23 15:00:57 +0200

    pret push avant merge partiel avec User-management branch

--- backend/django/django_web_app/merchex/jess/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    id = models.AutoField(primary_key=True) #id est une colonne qui existe par default dans Django
-    name = models.fields.CharField(max_length=100, null=True)
-    nickname = models.CharField(max_length=100, null=True)
-
--- backend/django/django_web_app/merchex/jess/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def jess_view(request):
-    return HttpResponse("Hello from Jess!")
