d6a701eccf54c41f5f5d6d24a6f8bd7f8faeabf4
Jessica ROUILLON <jrouillo@paul-f4Br5s2.clusters.42paris.fr>
2024-07-25 13:50:43 +0200

    derniers ajouts pour echange de donnees

--- user_management_container/user_management/api/views.py
+++ user_management_container/user_management/api/views.py
@@ -1,19 +1,36 @@
-from rest_framework.response import Response
 #converts any response to json
 
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 from base.models import CustomUser
 from .serializers import CustomUserSerializer
 
+
+
+# Retrieves a list of all CustomUser instances, all users'data
 @api_view(['GET'])
 def getData(request):
 	items = CustomUser.objects.all()
 	serializer = CustomUserSerializer(items, many=True)
 	return Response(serializer.data)
 
+
+# For user registration
 @api_view(['POST'])
 def addUser(request):
 	serializer = CustomUserSerializer(data=request.data)
 	if serializer.is_valid():
 		serializer.save()
 	return Response(serializer.data)
+
+
+
+# Retrieves the profile of the currently authenticated user
+
+class UserProfileView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+    def get_object(self):
+        return self.request.user
