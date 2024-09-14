b43e73cc121825d5b64fba7a9033bebea15ef9dc
Octo <cbsomarriba@gmail.com>
2024-09-14 11:15:14 +0200

    repartir de dashboard et reprendre la logique de new_dashboard petit à petit car 502 bad gateway est réglé

--- src/dashboard/dashboard/api/urls.py
+++ src/dashboard/dashboard/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-""" urlpatterns = [
-	path('api/getData/', views.getData, name='get_data'),
-	path('add/', views.addStats),
-	path('', views.getData),
-] """
+# urlpatterns = [
+# 	path('api/getData/', views.getData, name='get_data'),
+# 	path('add/', views.addStats),
+# 	path('', views.getData),
+# ]
 
 
 urlpatterns = [
