b06b5cc7b0d3f14a96490bbb7f6c987efcde8b06
Clement BERNAZEAU <cbernaze@made-f0Dr9s7.clusters.42paris.fr>
2024-09-25 18:40:46 +0200

    quelques rectification apres le merge pour corriger des bugs avant de push sur le main

--- src/user/user_management/user_management_database/urls.py
+++ src/user/user_management/user_management_database/urls.py
@@ -25,8 +25,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
 	# path('admin/', admin.site.urls),
-	path('api/', include('api.urls')),
-	path('api/users/', include('api.urls')),
+	path('api/', include('api_user.urls')),
+	path('api/users/', include('api_user.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 	path('metrics/', exports.ExportToDjangoView, name='metrics')
